dfb6ab05a5b183b3bcb921a965506ee7ef671fab
shaan <shaanyworkspace@gmail.com>
2025-12-21 00:23:52 +0530

    Configuration

--- src/main/java/com/therapistms/Configuration/RestTempLoadBalancer.java
+++ src/main/java/com/therapistms/Configuration/RestTempLoadBalancer.java
@@ -1,6 +1,5 @@
 package com.therapistms.Configuration;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTempLoadBalancer {
 
     @Bean
-    @LoadBalanced
+
     public RestTemplate restTemplate (){
         return new RestTemplate();
     }
