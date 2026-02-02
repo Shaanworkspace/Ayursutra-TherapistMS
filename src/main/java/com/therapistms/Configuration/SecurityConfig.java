0132435f5bdd4af24bb11e55c7dd113338e6ada8
shaan <shaanyworkspace@gmail.com>
2026-02-02 19:06:47 +0530

    CORS for deployment

--- src/main/java/com/therapistms/Configuration/SecurityConfig.java
+++ src/main/java/com/therapistms/Configuration/SecurityConfig.java
@@ -21,8 +21,6 @@ public class SecurityConfig {
 	private final JwtAuthenticationFilter jwtAuthFilter;
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-
 		http
 				.csrf(AbstractHttpConfigurer::disable)
 				.cors(AbstractHttpConfigurer::disable)
@@ -32,7 +30,7 @@ public class SecurityConfig {
 						)
 				)
 				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/api/patients/health").permitAll()
+						.requestMatchers("/api/therapists/health").permitAll()
 						.anyRequest().authenticated()
 				)
 				.addFilterBefore(
