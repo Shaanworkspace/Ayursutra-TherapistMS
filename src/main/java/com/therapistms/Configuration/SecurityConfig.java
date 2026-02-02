965ab2a46a683662c439c1d871b1d4d226f7cc56
shaan <shaanyworkspace@gmail.com>
2026-02-02 15:08:15 +0530

    Improvements for deployment

--- src/main/java/com/therapistms/Configuration/SecurityConfig.java
+++ src/main/java/com/therapistms/Configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,6 +25,7 @@ public class SecurityConfig {
 
 		http
 				.csrf(AbstractHttpConfigurer::disable)
+				.cors(Customizer.withDefaults())
 				.sessionManagement(session ->
 						session.sessionCreationPolicy(
 								SessionCreationPolicy.STATELESS
