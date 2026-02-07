18a261f3ce3838ad5c20f7c9c23e765362307730
shaan <shaanyworkspace@gmail.com>
2026-02-08 01:20:57 +0530

    CORS for https://ayursutra.me

--- src/main/java/com/therapistms/Configuration/SecurityConfig.java
+++ src/main/java/com/therapistms/Configuration/SecurityConfig.java
@@ -26,6 +26,8 @@ public class SecurityConfig {
 			"https://ayursutra-frontend.netlify.app",
 			"http://localhost:5173",
 			"http://localhost:3000",
+			"https://ayursutra.me",
+			"https://www.ayursutra.me",
 			"http://127.0.0.1:5173",
 			"http://127.0.0.1:3000"
 	);
